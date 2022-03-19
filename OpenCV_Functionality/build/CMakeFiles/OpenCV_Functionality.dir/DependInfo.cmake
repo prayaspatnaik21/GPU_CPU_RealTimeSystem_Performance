8bbe9bb4b8d614abfa949d7f4b2fe4365a8ccba5
Prayas <prayaspatnaik21@gmail.com>
2022-03-18 22:05:52 -0500

    OpenCV installations and Folder Structure Change

---
+++ OpenCV_Functionality/build/CMakeFiles/OpenCV_Functionality.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/prayas/Home/UTD_SPRING_2022/CS_6396_RealTimeSystem/Projects/GPU_CPU_RealTimeSystem_Performance/OpenCV_Functionality/OpenCV_Functionality.cpp" "/home/prayas/Home/UTD_SPRING_2022/CS_6396_RealTimeSystem/Projects/GPU_CPU_RealTimeSystem_Performance/OpenCV_Functionality/build/CMakeFiles/OpenCV_Functionality.dir/OpenCV_Functionality.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpenCV_Functionality/build/CMakeFiles/OpenCV_Functionality.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCV_Functionality.dir/OpenCV_Functionality.cpp.o"
+  "OpenCV_Functionality"
+  "OpenCV_Functionality.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCV_Functionality.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
